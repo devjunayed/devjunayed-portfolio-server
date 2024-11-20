f7d21718345b38e695702c3653f614a5c752ad60
devjunayed <devjunayed@gmail.com>
2024-11-20 21:32:21 +0600

    server edited

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,17 @@
-import app from './app';
-import config from './app/config';
+import app from "./app";
+import config from "./app/config";
+import {Server} from 'http';
 
+let server: Server;
 
-app.listen(config.port, () => {
-    console.log(`Server is running on port: ${config.port}`)
-})
+async function main() {
+  try {
+    server = app.listen(config.port, () => {
+      console.log(`Server is running on port: ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
